64d220f9abab07c0630578e702f556a10db58ca5
steshkof <steshkof@gmail.com>
2022-05-17 08:56:13 +0300

    #2

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
+import { strict as assert } from 'assert';
 import capitalize from "../src/capitalize.js";
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//     throw new Error('Функция работает неверно!');
+// }
 
-if (capitalize('') !== '') {
-throw new Error('Функция работает неверно!');
-}
+assert(capitalize('hello') === 'Hello');
+assert.equal(capitalize('hello'), 'Hello');
+
+// if (capitalize('') !== '') {
+// throw new Error('Функция работает неверно!');
+// }
+assert(capitalize('') === '');
+assert.deepEqual(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
